421f1c45b7f7f04ef30d98b1f86f33e7c70ed8c8
Luiz dos Reis <luizdosreis@gmail.com>
2017-10-10 22:58:07 -0300

    Reconfigurando o WebSecurityConfigurerAdapter

--- src/main/java/br/com/springboot/controleorcamento/controleorcamento/security/SecurityConfig.java
+++ src/main/java/br/com/springboot/controleorcamento/controleorcamento/security/SecurityConfig.java
@@ -2,8 +2,6 @@ package br.com.springboot.controleorcamento.controleorcamento.security;
 
 import br.com.springboot.controleorcamento.controleorcamento.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,32 +10,26 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-
-import static br.com.springboot.controleorcamento.controleorcamento.security.SecurityConstants.SIGN_UP_URL;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-	@Autowired
 	private UsuarioService usuarioService;
 
-
+	@Autowired
+	public SecurityConfig(UsuarioService usuarioService) {
+		this.usuarioService = usuarioService;
+	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception{
 		http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
 				.and().csrf().disable()
 				.authorizeRequests()
-				//.antMatchers(HttpMethod.GET,SIGN_UP_URL).permitAll()
-               // .antMatchers("v1/gastos/protected").access("hasRole('ROLE_USER')")
-               // .antMatchers("/*/admin/**").hasRole("ADMIN")
-                //.antMatchers(/*/user/**"").permitAll()
-                //.antMatchers(HttpMethod.OPTIONS).permitAll()
 				.antMatchers(HttpMethod.POST,"/*/user/**").permitAll()
-				.anyRequest().authenticated()
+				.antMatchers("/v1/**").authenticated()
                 .and()
 				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
 				.addFilter(new JWTAuthorizationFilter(authenticationManager(),usuarioService));
